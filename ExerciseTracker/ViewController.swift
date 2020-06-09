be5ccd51a78d89a31e183949775fa4c84471a8ae
Maya Itty <mni.maya04@gmail.com>
2020-06-08 17:23:43 -0700

    Initial Commit

---
+++ ExerciseTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExerciseTracker
+//
+//  Created by Maya Itty on 6/8/20.
+//  Copyright © 2020 Maya Itty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
